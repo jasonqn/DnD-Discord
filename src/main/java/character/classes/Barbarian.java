9d59832df4be7a7d9b87bcaeedda6913af5a23de
jasonqn <jasonqn94@gmail.com>
2023-01-18 20:57:48 +0000

    added classes as own class

---
+++ src/main/java/character/classes/Barbarian.java
@@ -0,0 +1,11 @@
+package character.classes;
+
+public class Barbarian {
+
+    private int dice = 1;
+    private int diceSides = 12;
+    private int level = 0;
+
+
+
+}
---
+++ src/main/java/character/classes/Fighter.java
@@ -0,0 +1,4 @@
+package character.classes;
+
+public class Fighter {
+}
