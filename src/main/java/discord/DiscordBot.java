9e15914030d3e51e79d31419ebbde18ed8c46f5d
jasonqn <jasonqn94@gmail.com>
2023-01-18 22:20:15 +0000

    Updated POM for JDA & created Discord class

---
+++ src/main/java/discord/DiscordBot.java
@@ -0,0 +1,16 @@
+package discord;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+
+
+public class DiscordBot {
+
+    public static void main(String[] arguments) throws Exception {
+
+        JDA api = JDABuilder.createDefault("")
+                .enableIntents(GatewayIntent.MESSAGE_CONTENT)
+                .build();
+    }
+}
